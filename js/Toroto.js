e91870296f4578fa72f6139a182e7388762ea885
kidceg <31203059+kidceg@users.noreply.github.com>
2017-09-22 23:02:10 +0800

    add css

--- js/Toroto.js
+++ js/Toroto.js
@@ -1,22 +1,53 @@
-$(document).ready(function () {
+$( window ).on( "load", function(){
+ 
+//   waterfall('imgs');
+//   var dataInt = {'data':[{'src':'1 (1).jpg'},{'src':'1 (2).jpg'},{'src':'1 (31).jpg'},{'src':'1 (4).jpg'},{'src':'1 (6).jpg'},{'src':'1 (7).jpg'},{'src':'1 (8).jpg'},{'src':'1 (9).jpg'},{'src':'1 (10).jpg'},{'src':'1 (11).jpg'},{'src':'1 (12).jpg'},{'src':'1 (13).jpg'},{'src':'1 (14).jpg'},{'src':'1 (15).jpg'},{'src':'1 (16).jpg'},{'src':'1 (17).jpg'},{'src':'1 (18).jpg'},{'src':'1 (19).jpg'},{'src':'1 (20).jpg'}]};
 
-   // 让轮播图的高与宽有比例为0.6自适应
-	var $s_pH = $(".s_pic div").width()*0.6;
-	$(".s_pic div").height($s_pH);
-   // 让轮播图小图标的高与宽有比例为0.6自适应
-	var $b_pH = $(".b_pic").width()*0.6;
-	$(".b_pic").height($b_pH);
-
-// 每次窗口变小的时候重新加载宽高，固定窗口的设备可删除这段
-$(window).resize(function(){
-	var $s_pH = $(".s_pic div").width()*0.6;
-	$(".s_pic div").height($s_pH);
-	var $b_pH = $(".b_pic").width()*0.6;
-	$(".b_pic").height($b_pH);
-});
-
+//   Window.onscroll = function() {
+//   	if (checkScroll()) {
+//   		$.each( dataInt.data,function(index, value) {
+//   			var $obox = $('<div>').addClass('box').appendTo($('#imgs'));
+//   			var $osmbox = $('<div>').addClass('smbox').appendTo($obox);
+//   			$('<img>').attr('src','./images/' + $(value).attr('src')).appendTo($osmbox);
+//   		});
+//   		waterfall();
+//   	}
+//   } 
 
 
+//  function waterfall(imgs) {
+//  	var $boxs = $('#imgs').children();
+//  	var $boxW = $boxs.eq(0).outerWidth();
+//     var cols = Math.floor($("#imgs").width() / $boxW);
+//     console.log(cols);
+//  	var arrH = [];
+//  	$boxs.each(function(index,value) {
+//  		var boxH = $boxs.eq(index).outerHeight();
+//  		if (index < cols) {
+//           arrH.push(boxH);
+//  		} else {		
+//           var minH = Math.min.apply(null,arrH);
+//           var minH_index = $.inArray(minH, arrH);
+//           $boxs.eq(index).css({
+//           	'position': 'abosulte',
+//           	'top': minH,
+//           	'left': minH_index * $boxW
+//           });
+//           arrH[minH_index] += $boxs.eq(index).outerHeight();
+//      console.log($boxs.eq(index).offset().top);
+//  		}
+//  	});
+//  } 
+
+
+// function checkScroll() {
+// 	var $boxs = $('#imgs').children();
+// 	var lastBoxH = $boxs.last().offsetTop + $boxs.last().outerHeight()/2;
+// 	var scrollTop = $(window).scrollTop();
+// 	var documentH = $(document).width();
+// 	return (lastBoxH < scrollTop + documentH) ?
+// 	true : false;
+// }
 
 
 
